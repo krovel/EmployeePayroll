2fbd08d4221b2653fa6fd4f56e0d5e87ecb6d1f4
krove <krove@DESKTOP-Q5SED1R>
2020-11-06 01:48:52 +0530

    [Kashif]Added:Project to implement new complete ER model and encapsulated all the required fields in the EmployeePayrollData pojo class

--- src/main/java/com/cg/EmployeePayrollData.java
+++ src/main/java/com/cg/EmployeePayrollData.java
@@ -1,14 +1,18 @@
 package com.cg;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class EmployeePayrollData {
 
 	private int employeeId;
 	private String employeeName;
 	private double salary;
-	private LocalDate startDate;
+	private List<LocalDate> startDates;
 	private String gender;
+	private String companyName;
+	private List<String> phoneNumbers;
+	private List<String> departmentNames;
 
 	public EmployeePayrollData() {
 	}
@@ -19,15 +23,24 @@ public class EmployeePayrollData {
 		this.salary = salary;
 	}
 
-	public EmployeePayrollData(int employeeId, String employeeName, double salary, LocalDate startDate) {
+	public EmployeePayrollData(int employeeId, String employeeName, double salary, List<LocalDate> startDates) {
 		this(employeeId, employeeName, salary);
-		this.startDate = startDate;
+		this.startDates = startDates;
 	}
 
 	
-	public EmployeePayrollData(int employeeId, String employeeName, double salary, LocalDate startDate, String gender) {
-		this(employeeId, employeeName, salary, startDate);
+	public EmployeePayrollData(int employeeId, String employeeName, double salary, List<LocalDate> startDates, String gender) {
+		this(employeeId, employeeName, salary, startDates);
 		this.gender = gender;
+	}
+	
+
+	public EmployeePayrollData(int employeeId, String employeeName, double salary, List<LocalDate> startDates,
+			String gender, String companyName, List<String> phoneNumbers, List<String> departmentNames) {
+		this(employeeId, employeeName, salary, startDates, gender);
+		this.companyName = companyName;
+		this.phoneNumbers = phoneNumbers;
+		this.departmentNames = departmentNames;
 	}
 
 	public int getEmployeeId() {
@@ -54,14 +67,6 @@ public class EmployeePayrollData {
 		this.salary = salary;
 	}
 
-	public LocalDate getStartDate() {
-		return startDate;
-	}
-
-	public void setStartDate(LocalDate startDate) {
-		this.startDate = startDate;
-	}
-
 	public String getGender() {
 		return gender;
 	}
@@ -70,10 +75,56 @@ public class EmployeePayrollData {
 		this.gender = gender;
 	}
 
+	public List<LocalDate> getStartDates() {
+		return startDates;
+	}
+
+	public void setStartDates(List<LocalDate> startDates) {
+		this.startDates = startDates;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public List<String> getPhoneNumbers() {
+		return phoneNumbers;
+	}
+
+	public void setPhoneNumbers(List<String> phoneNumbers) {
+		this.phoneNumbers = phoneNumbers;
+	}
+
+	public List<String> getDepartmentNames() {
+		return departmentNames;
+	}
+
+	public void setDepartmentNames(List<String> departmentNames) {
+		this.departmentNames = departmentNames;
+	}
+
 	@Override
 	public String toString() {
 		return "Id : " + employeeId + "\t" + "Name : " + employeeName + "\t" + "Gender : " + gender + "\t" + "Salary : "
-				+ salary + "\t" + "Start Date : " + startDate;
+				+ salary + "\t" + "Company Name : " + companyName;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((companyName == null) ? 0 : companyName.hashCode());
+		result = prime * result + employeeId;
+		result = prime * result + ((employeeName == null) ? 0 : employeeName.hashCode());
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(salary);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
 	}
 
 	@Override
@@ -85,6 +136,11 @@ public class EmployeePayrollData {
 		if (getClass() != obj.getClass())
 			return false;
 		EmployeePayrollData other = (EmployeePayrollData) obj;
+		if (companyName == null) {
+			if (other.companyName != null)
+				return false;
+		} else if (!companyName.equals(other.companyName))
+			return false;
 		if (employeeId != other.employeeId)
 			return false;
 		if (employeeName == null) {
@@ -92,13 +148,13 @@ public class EmployeePayrollData {
 				return false;
 		} else if (!employeeName.equals(other.employeeName))
 			return false;
+		if (gender == null) {
+			if (other.gender != null)
+				return false;
+		} else if (!gender.equals(other.gender))
+			return false;
 		if (Double.doubleToLongBits(salary) != Double.doubleToLongBits(other.salary))
 			return false;
-		if (startDate == null) {
-			if (other.startDate != null)
-				return false;
-		} else if (!startDate.equals(other.startDate))
-			return false;
 		return true;
-	}
+	}	
 }
